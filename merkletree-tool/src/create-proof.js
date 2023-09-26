52bb1249ad360171d9b3f72d2d258a09ad4fd51e
codeesura <120671243+codeesura@users.noreply.github.com>
2023-09-26 16:18:23 +0300

    Update create-proof.js

--- merkletree-tool/src/create-proof.js
+++ merkletree-tool/src/create-proof.js
@@ -1,141 +1,84 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 const { ethers } = require("ethers");
 const { MerkleTree } = require("merkletreejs");
 
-const nftFreelist = require("./nft-freelist.json");
-const nftWhitelist = require("./nft-whitelist.json");
-const tokenAirdroplist = require("./token-airdroplist.json");
-const tokenSeedlist = require("./token-seedlist.json");
-const tokenPrelist = require("./token-prelist.json");
+const inputFiles = [
+  "./nft-freelist.json",
+  "./nft-whitelist.json",
+  "./token-airdroplist.json",
+  "./token-seedlist.json",
+  "./token-prelist.json",
+];
+
 const outDirectory = "./out";
 
-const keccak256 = ethers.keccak256;
-
-let nftFreelistOut = [];
-let nftWhitelistOut = [];
-let tokenFreelistOut = [];
-let tokenSeedlistOut = [];
-let tokenPrelistOut = [];
-
-function forNFTFreelist() {
-  const leafNodes = nftFreelist.map((addr) =>
-    keccak256(Buffer.concat([Buffer.from(addr.replace("0x", ""), "hex")]))
-  );
-
-  const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
-
-  for (let i = 0; i < nftFreelist.length; i++) {
-    nftFreelistOut.push({
-      address: nftFreelist[i],
-      proof: merkleTree.getHexProof(leafNodes[i]),
-    });
+async function checkFilesExistence(files) {
+  for (const file of files) {
+    try {
+      await fs.access(file);
+    } catch (e) {
+      console.error(`${file} does not exist.`);
+      process.exit(1);
+    }
   }
-  fs.writeFileSync(
-    path.join(outDirectory, `nft-freelist-proofs.json`),
-    JSON.stringify({
-      merkleRoot: merkleTree.getHexRoot(),
-      proofs: nftFreelistOut,
-    })
-  );
 }
 
-function forNFTWhitelist() {
-  const leafNodes = nftWhitelist.map((addr) =>
-    keccak256(Buffer.concat([Buffer.from(addr.replace("0x", ""), "hex")]))
-  );
-
-  const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
-
-  for (let i = 0; i < nftWhitelist.length; i++) {
-    nftWhitelistOut.push({
-      address: nftWhitelist[i],
-      proof: merkleTree.getHexProof(leafNodes[i]),
-    });
-  }
-  fs.writeFileSync(
-    path.join(outDirectory, `nft-whitelist-proofs.json`),
-    JSON.stringify({
-      merkleRoot: merkleTree.getHexRoot(),
-      proofs: nftWhitelistOut,
-    })
-  );
+async function readJsonFile(filePath) {
+  const content = await fs.readFile(filePath, "utf-8");
+  return JSON.parse(content);
 }
 
-function forTokenAirdroplist() {
-  const leafNodes = tokenAirdroplist.map((addr) =>
-    keccak256(Buffer.concat([Buffer.from(addr.replace("0x", ""), "hex")]))
+async function generateProofs(list, fileName) {
+  const leafNodes = list.map((addr) =>
+    ethers.utils.keccak256(
+      Buffer.concat([Buffer.from(addr.replace("0x", ""), "hex")])
+    )
   );
+  const merkleTree = new MerkleTree(leafNodes, ethers.utils.keccak256, {
+    sortPairs: true,
+  });
 
-  const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+  const proofsOut = list.map((address, index) => ({
+    address,
+    proof: merkleTree.getHexProof(leafNodes[index]),
+  }));
 
-  for (let i = 0; i < tokenAirdroplist.length; i++) {
-    tokenFreelistOut.push({
-      address: tokenAirdroplist[i],
-      proof: merkleTree.getHexProof(leafNodes[i]),
-    });
-  }
-  fs.writeFileSync(
-    path.join(outDirectory, `token-airdrop-proofs.json`),
+  await fs.writeFile(
+    path.join(outDirectory, fileName),
     JSON.stringify({
       merkleRoot: merkleTree.getHexRoot(),
-      proofs: tokenFreelistOut,
+      proofs: proofsOut,
     })
   );
+
+  console.log(`${fileName} has been written.`);
 }
 
-function forTokenSeedlist() {
-  const leafNodes = tokenSeedlist.map((addr) =>
-    keccak256(Buffer.concat([Buffer.from(addr.replace("0x", ""), "hex")]))
-  );
+async function main() {
+  await checkFilesExistence([...inputFiles, outDirectory]);
 
-  const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
-
-  for (let i = 0; i < tokenSeedlist.length; i++) {
-    tokenSeedlistOut.push({
-      address: tokenSeedlist[i],
-      proof: merkleTree.getHexProof(leafNodes[i]),
-    });
+  if (!(await fs.stat(outDirectory).catch(() => fs.mkdir(outDirectory)))) {
+    console.log(`Created output directory: ${outDirectory}`);
   }
-  fs.writeFileSync(
-    path.join(outDirectory, `token-seedsale-proofs.json`),
-    JSON.stringify({
-      merkleRoot: merkleTree.getHexRoot(),
-      proofs: tokenSeedlistOut,
-    })
-  );
-}
 
-function forTokenPrelist() {
-  const leafNodes = tokenPrelist.map((addr) =>
-    keccak256(Buffer.concat([Buffer.from(addr.replace("0x", ""), "hex")]))
-  );
+  const [
+    nftFreelist,
+    nftWhitelist,
+    tokenAirdroplist,
+    tokenSeedlist,
+    tokenPrelist,
+  ] = await Promise.all(inputFiles.map(readJsonFile));
 
-  const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
-
-  for (let i = 0; i < tokenPrelist.length; i++) {
-    tokenPrelistOut.push({
-      address: tokenPrelist[i],
-      proof: merkleTree.getHexProof(leafNodes[i]),
-    });
-  }
-  fs.writeFileSync(
-    path.join(outDirectory, `token-presale-proofs.json`),
-    JSON.stringify({
-      merkleRoot: merkleTree.getHexRoot(),
-      proofs: tokenPrelistOut,
-    })
-  );
-}
-
-try {
-  forNFTFreelist();
-  forNFTWhitelist();
-  forTokenAirdroplist();
-  forTokenSeedlist();
-  forTokenPrelist();
+  await Promise.all([
+    generateProofs(nftFreelist, "nft-freelist-proofs.json"),
+    generateProofs(nftWhitelist, "nft-whitelist-proofs.json"),
+    generateProofs(tokenAirdroplist, "token-airdrop-proofs.json"),
+    generateProofs(tokenSeedlist, "token-seedsale-proofs.json"),
+    generateProofs(tokenPrelist, "token-presale-proofs.json"),
+  ]);
 
   console.log("Done!");
-} catch (e) {
-  console.log(e);
 }
+
+main().catch((e) => console.error(e));
